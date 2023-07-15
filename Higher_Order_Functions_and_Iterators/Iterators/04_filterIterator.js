8e085ec7c4681e9218680cc855aa2c104f5f9f3e
Chad <chadb78999@gmail.com>
2023-07-15 08:07:30 -0500

    Worked on filter

---
+++ Higher_Order_Functions_and_Iterators/Iterators/04_filterIterator.js
@@ -0,0 +1,22 @@
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+const smallNumbers=randomNumbers.filter(num=>{return num<250});
+console.log(smallNumbers);
+
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+const favlongWords=favoriteWords.filter(word=>{return word.length>7});
+console.log(favlongWords);
+
+
+// some additional testing
+const mixedRandom = [[1,2], [2,4], ["Testing","Testing"], ["5","500",23], ["Watch","this",100]];
+console.log(`\nTrying something with forEach and filter\nmixedRandom: ${mixedRandom}\ncreating empty nums and strings array\n`);
+let nums =[];
+let strings =[];
+//the below works but I forgot to use filter...
+//mixedRandom.forEach(element=>{if(Number.isInteger(element)){nums.push(element)}else{strings.push(element)}});
+//mixedRandom.forEach(element=>{element.filter(ele=>{if(Number.isInteger(ele)){nums.push(ele)}else{strings.push(elet)}}}));
+// The below works but it was a pain to type...
+mixedRandom.forEach(element=>{element.filter(e=>{if(Number.isInteger(e)){nums.push(e)}else{strings.push(e)}})})
+console.log(`\n\n\ndoes it work?\nnums: ${nums}\nstrings: ${strings}`);
