f5c1b87addc61e73f14b44d8dcb2cb1ebe4f0f44
Chad <chadb78999@gmail.com>
2023-07-16 07:26:30 -0500

    added findIndex

---
+++ Higher_Order_Functions_and_Iterators/Iterators/05_findIndex.js
@@ -0,0 +1,3 @@
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+const foundAnimal=animals.findIndex(animal=>{if(animal==="elephant"){return animal;}});
+console.log(foundAnimal);
