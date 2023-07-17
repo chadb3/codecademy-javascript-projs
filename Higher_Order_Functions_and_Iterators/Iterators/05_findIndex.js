b2370a825907b609f47bb77bf18f6ff981b78a99
Chad <chadb78999@gmail.com>
2023-07-17 01:11:09 -0500

    finished findIndex

--- Higher_Order_Functions_and_Iterators/Iterators/05_findIndex.js
+++ Higher_Order_Functions_and_Iterators/Iterators/05_findIndex.js
@@ -1,3 +1,25 @@
 const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
 const foundAnimal=animals.findIndex(animal=>{if(animal==="elephant"){return animal;}});
 console.log(foundAnimal);
+//starts with s
+const startsWithS = animals.findIndex(animal=>{if(animal[0]==='s'){return animal;}});
+console.log(animals[startsWithS]);
+// my extra test to count the number of vowles in animals array
+const vowles = "aeiou";
+let animalVowles = []
+//loop through animals
+animals.findIndex(
+animal=>
+{
+	for(let i =0;i<vowles.length;i++)
+	{
+		if(animal.includes(vowles[i]))
+		{
+			//console.log("hit");
+			animalVowles.push(vowles[i]);
+			
+		}
+	}
+}
+);
+console.log(`number of vowles in \"animals\": ${animalVowles.length}`);
