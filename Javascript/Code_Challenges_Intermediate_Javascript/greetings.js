7065cc2dfd3af45989467c5375a160f0228d2e52
Chad <chadb78999@gmail.com>
2023-07-20 07:45:18 -0500

    one done w notes

---
+++ Javascript/Code_Challenges_Intermediate_Javascript/greetings.js
@@ -0,0 +1,10 @@
+/*
+* Write a function, greetAliens(), that takes in an array of strings and uses a for loop to print a greeting with each string in the array.
+*
+* The greeting should take the following format:
+*“ Oh powerful [stringElement], we humans offer our unconditional surrender!” 
+*/
+
+//cant use for each
+
+const dogs=["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
--- Javascript/Code_Challenges_Intermediate_Javascript/reverseArray.js
+++ Javascript/Code_Challenges_Intermediate_Javascript/reverseArray.js
@@ -1,21 +1,39 @@
 // Write your code here:
 const reverseArray=arrIn=>{
-	console.log("hit");
+	//console.log("hit");
+	if(Array.isArray(arrIn)){
+	// length of arrIn
+	const arrLen=arrIn.length;
+	// last index of arrIn
+	const lastIndex=arrLen-1;
+	// only need to go half way (otherwise we undo our work).
+	for(let i=0;i<lastIndex/2;i++)
+	{	
+		tmpVal=arrIn[i];
+		arrIn[i]=arrIn[lastIndex-i];
+		arrIn[lastIndex-i]=tmpVal;
+	}
+		// note the challenge has us return the array even though arrays are passed
+		// by reference and the original array is modified. 
+		// this is so the log statement works. (without it, it prints undefined)
+		return arrIn;
+	}else{console.log("Please try to input an array.\nsomething like: [1,2,3]."); return [arrIn];
+}
+
 }
 
 
 
 
 
-
-
 // When you're ready to test your code, uncomment the below and run:
-/* 
+ 
 const sentence = ['sense.','make', 'all', 'will', 'This'];
-
-console.log(reverseArray(sentence)) 
+// works
+console.log(reverseArray(sentence))
+// note that sentence has been modified
+console.log(sentence); 
 // Should print ['This', 'will', 'all', 'make', 'sense.'];
-*/
 
-reverseArray(['a','b','c']);
+//reverseArray(['a','b','c']);
 
