4562296fb73e5eb0983088b89038a24ffce43f89
Chad <chadb@Chads-MacBook-Pro.local>
2023-07-21 00:05:42 -0500

    Worked on random

---
+++ Random/forLoopNoForEachPractice.js
@@ -0,0 +1,9 @@
+const greetings=(arrIn)=>{
+	for(a in arrIn)
+	{
+		console.log(`Oh powerful ${arrIn[a]}, we humans offer our unconditional surrender!`);
+	}
+}
+
+const ain=["Bup","gray Alean", "Remdul","Frank"]
+greetings(ain)
