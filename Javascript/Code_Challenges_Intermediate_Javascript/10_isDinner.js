421b56787c78927579d0ee50e245a6b695e0c023
Chad <chadb78999@gmail.com>
2023-07-25 04:11:40 -0500

    renumbered files

---
+++ Javascript/Code_Challenges_Intermediate_Javascript/10_isDinner.js
@@ -0,0 +1,13 @@
+// This will take in an array of objects called dinner
+// name: name of the food
+// source: the type
+// I think they want plant based things returned 
+
+const isTheDinnerVegan=(arrIn)=>{
+
+}
+
+
+
+
+const dinner = [{name: 'hamburger', source: 'meat'}, {name: 'cheese', source: 'dairy'}, {name: 'ketchup', source:'plant'}, {name: 'bun', source: 'plant'}, {name: 'dessert twinkies', source:'unknown'}, {name: "Chips",source:"plant"}];
