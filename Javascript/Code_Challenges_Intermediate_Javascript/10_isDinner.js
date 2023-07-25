4829aa072347e4736a0d459ef1127c452b2bda2b
Chad <chadb78999@gmail.com>
2023-07-25 04:52:00 -0500

    worked on next challenge

--- Javascript/Code_Challenges_Intermediate_Javascript/10_isDinner.js
+++ Javascript/Code_Challenges_Intermediate_Javascript/10_isDinner.js
@@ -1,13 +1,20 @@
 // This will take in an array of objects called dinner
 // name: name of the food
 // source: the type
-// I think they want plant based things returned 
-
+// I think they want plant based things returned (source==="plant") 
+// This was covered in the "addtional iterators" lesson
+//
 const isTheDinnerVegan=(arrIn)=>{
-
+	//arrIn.forEach((l,r)=>console.log(`l:${l}\nr:${r}`))
+	return(arrIn.every(obj=>obj['source']==="plant"))
 }
 
 
 
 
-const dinner = [{name: 'hamburger', source: 'meat'}, {name: 'cheese', source: 'dairy'}, {name: 'ketchup', source:'plant'}, {name: 'bun', source: 'plant'}, {name: 'dessert twinkies', source:'unknown'}, {name: "Chips",source:"plant"}];
+const dinner = [{name: 'hamburger', source: 'meat'}, {name: 'cheese', source: 'dairy'}, {name: 'ketchup', source:'plant'}, {name: 'bun', source: 'plant'}, {name: 'dessert twinkies', source:'unknown'}];
+console.log(`This should return (false): ${isTheDinnerVegan(dinner)}`);
+
+// this should be true
+const dinner2 = [{name: 'arugula', source: 'plant'}, {name: 'tomatoes', source: 'plant'}, {name: 'lemon', source:'plant'}, {name: 'olive oil', source: 'plant'},{name:"chips",source:"plant"}];
+console.log(`This should return (true): ${isTheDinnerVegan(dinner2)}`);
