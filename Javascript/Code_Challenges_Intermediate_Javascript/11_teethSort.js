d85e02ca4e94ef3c4d3d66a4d5755a0687545ce4
Chad <chadb78999@gmail.com>
2023-07-25 05:50:09 -0500

    sort object

---
+++ Javascript/Code_Challenges_Intermediate_Javascript/11_teethSort.js
@@ -0,0 +1,12 @@
+// in this project, I will return the an object array with the objects sorted by the "numTeeth" value in acending order
+
+const speciesArray = [ {speciesName:'shark', numTeeth:50}, {speciesName:'dog', numTeeth:42}, {speciesName:'alligator', numTeeth:80}, {speciesName:'human', numTeeth:32}];
+// this needs to pass
+// console.log(orderByTeeth(speciesArray))
+// in other words it needs to return a sorted array
+const orderByTeeth=(arrIn)=>
+{
+	return arrIn.sort((l,r)=>l.numTeeth-r.numTeeth);
+}
+console.log(orderByTeeth(speciesArray));
+console.log(`old first index (should be shark): ${speciesArray[0]['speciesName']}`);
