07af86ff237fc68b027c6a52f68e89c29a98190a
Chad <chadb78999@gmail.com>
2023-08-08 06:43:32 -0500

    Traversing the DOM finished

---
+++ Javascript/Building_Interactive_Websites/03_Javascript_and_the_DOM/05_Traversing_the_DOM/main.js
@@ -0,0 +1,5 @@
+let first = document.querySelector('body').children[0];//.innerHTML="BROWN BEARS ARE AWESOME! (not really)";
+first.innerHTML="SDF";
+console.log(first);
+
+first.parentNode.style.backgroundColor="beige";
