c01b941ff3aa00223da594d36cd76c56e0e6d338
Chad <chadb78999@gmail.com>
2023-08-10 07:16:42 -0500

    slight correction

--- Javascript/Building_Interactive_Websites/03_Javascript_and_the_DOM/05_Traversing_the_DOM/main.js
+++ Javascript/Building_Interactive_Websites/03_Javascript_and_the_DOM/05_Traversing_the_DOM/main.js
@@ -1,5 +1,5 @@
 let first = document.querySelector('body').children[0];//.innerHTML="BROWN BEARS ARE AWESOME! (not really)";
-first.innerHTML="SDF";
+first.innerHTML="BROWN BEARS ARE AWESOME! (not really)";
 console.log(first);
 
 first.parentNode.style.backgroundColor="beige";
