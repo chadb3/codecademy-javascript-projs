b9182048c3ace2e7ddf8d1c10df74b97c52bb6a6
Chad <chadb78999@gmail.com>
2023-08-20 08:18:38 -0500

    a little more progress

--- Projects/Piano_Keys/main.js
+++ Projects/Piano_Keys/main.js
@@ -54,7 +54,7 @@ nextOne.onclick=function(){nextTwo.hidden=false;nextOne.hidden=true; document.ge
 
 // Write anonymous event handler property and function for the second progress button
 
-nextTwo.onclick=function(){nextThree.hidden=false;nextTwo.hidden=true;document.getElementById('word-five').innerHTML="DEAR"; document.getElementById('word-six').innerHTML="FRI-"; lastLyric.innerHTML="-END"; lastLyric.style.display='inline-block';}
+nextTwo.onclick=function(){nextThree.hidden=false;nextTwo.hidden=true;document.getElementById('word-five').innerHTML="DEAR"; document.getElementById('word-six').innerHTML="FRI-"; lastLyric.innerHTML="-END"; lastLyric.style.display='inline-block';document.getElementById('letter-note-three').innerHTML="G";document.getElementById('letter-note-four').innerHTML="E";document.getElementById('letter-note-five').innerHTML="C";document.getElementById('letter-note-six').innerHTML="B";}
 // Write anonymous event handler property and function for the third progress button
 nextThree.onclick=()=>{startOver.hidden=false;nextThree.hidden=true;}
 
