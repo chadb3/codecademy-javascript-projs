b137bc08a25b6a47cad452c6a8f6ec892ac0793e
Chad <chadb78999@gmail.com>
2023-08-21 04:43:10 -0500

    Piano proj now finished: fixed displaying hidden element

--- Projects/Piano_Keys/main.js
+++ Projects/Piano_Keys/main.js
@@ -54,9 +54,9 @@ nextOne.onclick=function(){nextTwo.hidden=false;nextOne.hidden=true; document.ge
 
 // Write anonymous event handler property and function for the second progress button
 
-nextTwo.onclick=function(){nextThree.hidden=false;nextTwo.hidden=true;document.getElementById('word-five').innerHTML="DEAR"; document.getElementById('word-six').innerHTML="FRI-"; lastLyric.innerHTML="-END"; lastLyric.style.display='inline-block';document.getElementById('letter-note-three').innerHTML="G";document.getElementById('letter-note-four').innerHTML="E";document.getElementById('letter-note-five').innerHTML="C";document.getElementById('letter-note-six').innerHTML="B";}
+nextTwo.onclick=function(){nextThree.hidden=false;nextTwo.hidden=true;document.getElementById('word-five').innerHTML="DEAR"; document.getElementById('word-six').innerHTML="FRI-"; /*lastLyric.innerHTML="-END";*/ lastLyric.style.display='inline-block';document.getElementById('letter-note-three').innerHTML="G";document.getElementById('letter-note-four').innerHTML="E";document.getElementById('letter-note-five').innerHTML="C";document.getElementById('letter-note-six').innerHTML="B";}
 // Write anonymous event handler property and function for the third progress button
-nextThree.onclick=()=>{startOver.hidden=false;nextThree.hidden=true;}
+nextThree.onclick=()=>{startOver.hidden=false;nextThree.hidden=true;document.getElementById('word-one').innerHTML="HAP-";document.getElementById('word-two').innerHTML="PY";document.getElementById('word-three').innerHTML="BIRTH";document.getElementById("word-four").innerHTML="DAY";document.getElementById("word-five").innerHTML="TO";document.getElementById("word-six").innerHTML="YOU!";document.getElementById("letter-note-one").innerHTML="F";document.getElementById("letter-note-two").innerHTML="F";document.getElementById("letter-note-three").innerHTML="E";document.getElementById("letter-note-four").innerHTML="C";document.getElementById("letter-note-five").innerHTML="D";document.getElementById("letter-note-six").innerHTML="C";lastLyric.style.display="none";}
 
 // This is the event handler property and function for the startOver button
 startOver.onclick = function() {
