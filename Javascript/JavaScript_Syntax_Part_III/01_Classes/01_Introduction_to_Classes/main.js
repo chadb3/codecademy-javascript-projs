f4a4b6227b788b6a55807c67a0d9e4658cc8c446
Chad <chadb78999@gmail.com>
2023-09-05 00:27:35 -0500

    starting on class section

---
+++ Javascript/JavaScript_Syntax_Part_III/01_Classes/01_Introduction_to_Classes/main.js
@@ -0,0 +1,25 @@
+class Dog {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+  get behavior() {
+    return this._behavior;
+  }   
+
+  incrementBehavior() {
+    this._behavior ++;
+  }
+}
+
+const halley = new Dog('Doggie');
+console.log(halley.name); 
+console.log(halley.behavior); 
+halley.incrementBehavior(); 
+console.log(halley.name); 
+console.log(halley.behavior); 
+
