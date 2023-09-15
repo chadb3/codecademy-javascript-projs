6aa1f21dd125c454d04ccec178db2fcea1dd24cd
Chad <chadb78999@gmail.com>
2023-09-15 07:17:09 -0500

    working on project

--- Projects/Library_Project/library.js
+++ Projects/Library_Project/library.js
@@ -2,5 +2,16 @@
 class Media{
 constructor(title){
 this._title=title;
+this._isCheckedOut=false;
+this._ratings=[];
+}
+get title(){
+ return this._title;
+}
+get isCheckedOut(){
+ return this._isCheckedOut;
+}
+get ratings(){
+ return this._ratings;
 }
 }
