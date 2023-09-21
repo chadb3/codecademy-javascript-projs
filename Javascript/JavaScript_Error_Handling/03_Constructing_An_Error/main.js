7ec17f887de17f51d9352ccedcbeac19713c744a
Chad <chadb78999@gmail.com>
2023-09-21 15:35:07 -0500

    working on error lessons

---
+++ Javascript/JavaScript_Error_Handling/03_Constructing_An_Error/main.js
@@ -0,0 +1,5 @@
+console.log(Error("User missing name"));
+
+
+console.log('Will logging the error stop our program from running?');
+
---
+++ Javascript/JavaScript_Error_Handling/03_Constructing_An_Error/tests.js
@@ -0,0 +1,5 @@
+const two=2;
+console.log(Error("TESTING"));
+console.log(two);
+
+console.log(new Error("Example Error"));
