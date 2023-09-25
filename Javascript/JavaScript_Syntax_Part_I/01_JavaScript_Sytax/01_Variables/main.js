38359f2ca22962d2d278440471a5382325b6c70e
Chad <chadb78999@gmail.com>
2023-09-25 08:02:37 -0500

    starting work on lessons since before starting this repo

---
+++ Javascript/JavaScript_Syntax_Part_I/01_JavaScript_Sytax/01_Variables/main.js
@@ -0,0 +1,11 @@
+// pre-ES6 versions of JavaScript use var
+var one=1;
+
+//let is now the preferred way to declair a variable. it can be reassigned.
+//const is used when the variable must remain constant and not be reassigned.
+
+let two=2;
+console.log(two);
+two="two";
+console.log(two);
+const important="important"
